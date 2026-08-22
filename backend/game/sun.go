a1dc830ed69cca8c6db388b8526cb53616b6fbed
Joel-Luca <joel.christinet@gmx.ch>
2026-08-23 01:41:14 +0200

    Add helper method to calc scale and radius

--- backend/game/sun.go
+++ backend/game/sun.go
@@ -1,5 +1,8 @@
 package game
 
+const SUN_GROWTH_RATE = 10.0
+const BLACK_HOLE_SHRINK_RATE = -100.0
+
 type Sun struct {
 	Radius             float32
 	Scale              float32
@@ -7,8 +10,10 @@ type Sun struct {
 	BlackHoleEndRadius float32
 }
 
-	BlackHoleStartRadius float32
-	BlackHoleMaxRadius   float32
+func (s *Sun) calc(progress float32, startRadius float32, shrinkRate float32) {
+	newRadius := s.Radius + shrinkRate*progress
+	s.Scale = newRadius / startRadius
+	s.Radius = newRadius
 }
 
 func (s *Sun) update(phase MatchPhase, phaseElapsed float32) {
@@ -18,20 +23,20 @@ func (s *Sun) update(phase MatchPhase, phaseElapsed float32) {
 		if progress > 1 {
 			progress = 1
 		}
-
-		s.Radius = s.StartRadius + (s.EndRadius-s.StartRadius)*progress
+		s.calc(progress, s.StartRadius, SUN_GROWTH_RATE)
 
 	case PhaseBlackHole:
 		if phaseElapsed <= BlackHoleCollapseDuration {
 			progress := phaseElapsed / BlackHoleCollapseDuration
-			s.Radius = s.EndRadius + (s.BlackHoleStartRadius-s.EndRadius)*progress
+
+			s.calc(progress, s.BlackHoleEndRadius, BLACK_HOLE_SHRINK_RATE)
 			return
 		}
 
 		growthElapsed := phaseElapsed - BlackHoleCollapseDuration
-		s.Radius = s.BlackHoleStartRadius + BlackHoleGrowthPerSecond*growthElapsed
-		if s.Radius > s.BlackHoleMaxRadius {
-			s.Radius = s.BlackHoleMaxRadius
+		s.calc(growthElapsed, s.BlackHoleEndRadius, BLACK_HOLE_SHRINK_RATE)
+		if s.Radius < s.BlackHoleEndRadius {
+			s.Radius = s.BlackHoleEndRadius
 		}
 	}
 }
